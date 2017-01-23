52ae205bf8d35fa4122ed2daa6d45cfca53054b7
Jacob Haig <jhaig343@gmail.com>
2017-01-23 10:58:40 -0500

    began tarot program
    will flesh it out later

---
+++ tarots.rb
@@ -0,0 +1,18 @@
+
+#TODO: add array with Arcana cards
+
+major_arcana = nil
+
+
+
+def minor_arcana(suit)
+		card = 1 + rand(9)
+		return "Your card is a #{card} of #{suit} "
+end
+chosen_suiot = nil
+
+
+
+
+
+
